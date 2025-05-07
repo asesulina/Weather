2d39bdce120d677326afdfeddf2b9cdc6fba0610
asesulina <asesulina@gmail.com>
2025-05-07 14:00:07 +0300

    Add files via upload

---
+++ Weather.API.Test/TestHelpers/TemperatureRanges.cs
@@ -0,0 +1,14 @@
+namespace Weather.API.Tests.TestHelpers
+{
+    public static class TemperatureRanges
+    {
+        public const int MetricMin = -50;
+        public const int MetricMax = 50;
+
+        public const int ImperialMin = -60;
+        public const int ImperialMax = 130;
+
+        public const int StandardMin = 180;
+        public const int StandardMax = 323;
+    }
+}
